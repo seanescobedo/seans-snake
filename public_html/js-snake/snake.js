60366d8d55b0626c5486b5c16c3ae9855b058857
sean escobedo <seancandoit68@gmail.com>
2014-10-07 09:19:42 -0700

    work 10/7/14 rell

--- public_html/js-snake/snake.js
+++ public_html/js-snake/snake.js
@@ -1,1 +1,21 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    canvas.width = screenWidth;
+}
+function gameLoop() {
+    
+}
+function gameDraw() {
+    
+}
 
