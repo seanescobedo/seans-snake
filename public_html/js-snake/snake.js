b44ee475f3d4d2479bbc0ebe892aaa90f539ae8c
sean escobedo <seancandoit68@gmail.com>
2014-10-07 21:31:06 -0700

    idk wtf the background color won't work ughh
    i dislike javascript alex -_-
    
    btw i won't be in tomorrow

--- public_html/js-snake/snake.js
+++ public_html/js-snake/snake.js
@@ -4,18 +4,23 @@ var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
     
     screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
+    
     canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 function gameLoop() {
     
 }
 function gameDraw() {
-    
+    context.fillStyle = "rgb(240, 14, 67)";
+    context.fillRect = ( 340, 94, screenWidth, screenHeight);
 }
 
